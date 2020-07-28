db047a60f9ba44ff7af1ab2ba462dde229db39a2
Sivaram-Chinnathambi <sivaram.hcl@live.com>
2020-07-28 22:43:45 +0530

    Updates

---
+++ precache-manifest.6efd61b3c51929311e458df6ef285827.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2a23eb953ae651533744353bfbe1ab1",
+    "url": "/react-host-test/index.html"
+  },
+  {
+    "revision": "dd6e1cbdcdf0c81c75b9",
+    "url": "/react-host-test/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4310b126a7185f2b5ec5",
+    "url": "/react-host-test/static/js/2.22c637bc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-host-test/static/js/2.22c637bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd6e1cbdcdf0c81c75b9",
+    "url": "/react-host-test/static/js/main.0d0647ac.chunk.js"
+  },
+  {
+    "revision": "4a5360ecf8addcbd588c",
+    "url": "/react-host-test/static/js/runtime-main.8a17bbeb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-host-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
